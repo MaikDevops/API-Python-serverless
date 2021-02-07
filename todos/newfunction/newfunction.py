1a0e753c45c877861a1ef1aa7d47814414de5ade
MaikDevops <77570209+MaikDevops@users.noreply.github.com>
2021-02-07 12:38:45 +0100

    newfunction

---
+++ todos/newfunction/newfunction.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+from todos import decimalencoder
+dynamodb = boto3.resource('dynamodb')
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
