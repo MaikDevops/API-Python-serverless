7e5177d1254e4540aa32af21532c2d3ca8d50f4d
MaikDevops <77570209+MaikDevops@users.noreply.github.com>
2021-02-09 10:05:10 +0100

    New function translate

---
+++ todos/getTranslate.py
@@ -0,0 +1,6 @@
+import json
+import boto3
+import os
+
+translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+resultTx = translate.translate_text(Text='Hello, World', SourceLanguageCode='en', TargetLanguageCode='es')
